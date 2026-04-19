91dab791b29697316121b430e6d20fd404ba8315
nehemi <nehemiasaquino@gmail.com>
2026-04-18 23:41:40 -0300

    mejora de SEO

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()],
-	server: {
-		watch: {
-			// Ignoramos la carpeta de imágenes para que Vite no reinicie
-			// el servidor de desarrollo cada vez que subas un archivo.
-			ignored: ['**/static/images/**']
-		}
-	}
+  plugins: [tailwindcss(), sveltekit()],
+  server: {
+    allowedHosts: true, 
+    
+    watch: {
+      
+      ignored: ['**/static/images/**']
+    },
+  }
 });
